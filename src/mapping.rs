6e7672c91fc070788ca9e34597e89acbb9fc56db
nullchinchilla <nullchinchilla@pm.me>
2022-03-15 10:02:08 -0500

    fix windows builds

--- src/mapping.rs
+++ src/mapping.rs
@@ -41,7 +41,7 @@ impl Mapping {
         handle.seek(SeekFrom::Start(0))?;
 
         // Now it's safe to memmap the file, because it's EXCLUSIVELY locked to this process.
-        let mut table_mmap = unsafe { MmapOptions::new().offset(1 << 30).map_mut(&handle)? };
+        let table_mmap = unsafe { MmapOptions::new().offset(1 << 30).map_mut(&handle)? };
 
         let mut alloc_mmap = unsafe { MmapOptions::new().len(1 << 30).map_mut(&handle)? };
         #[cfg(target_os = "linux")]
